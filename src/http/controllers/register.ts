4bf884b376b8322bf3c2f3f6ae43c30e7f35a485
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-05-05 18:13:17 -0300

    Create hash password with bcrypt

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,5 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
+import { hash } from 'bcryptjs'
 import { prisma } from '@/libs/prisma'
 import { z } from 'zod'
 
@@ -11,11 +12,21 @@ export async function register(req: FastifyRequest, res: FastifyReply) {
 
   const { email, name, password } = registerBodySchema.parse(req.body)
 
+  const password_hash = await hash(password, 2)
+
+  const userExistis = await prisma.user.findUnique({
+    where: { email },
+  })
+
+  if (userExistis) {
+    return res.status(409).send({ message: 'Usuário já existe' })
+  }
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash,
     },
   })
 
