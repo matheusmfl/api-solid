ba459f90eca29ab23533500c761e45e0af5d3655
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-05-08 17:52:25 -0300

    Create Error Tratative on Fastify

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,23 @@
 import fastify from 'fastify'
 import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((err, _, res) => {
+  if (err instanceof ZodError) {
+    return res
+      .status(400)
+      .send({ message: 'Validation error', issues: err.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(err)
+  } else {
+    // TODO: Here we should log to an external tool like DataDog/Sentry/NewRelic
+  }
+  return res.status(500).send({ message: 'Internal server error' })
+})
