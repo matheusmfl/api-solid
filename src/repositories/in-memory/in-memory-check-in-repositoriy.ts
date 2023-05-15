2c4423a647c1d58f5d03ea7986eb84ca8090d4a8
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-05-15 13:42:26 -0300

    create use case get-user-metrics

--- src/repositories/in-memory/in-memory-check-in-repositoriy.ts
+++ src/repositories/in-memory/in-memory-check-in-repositoriy.ts
@@ -4,6 +4,7 @@ import { randomUUID } from 'node:crypto'
 import dayjs from 'dayjs'
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
+
   public items: CheckIn[] = []
   async findByUserIdOnDate(userId: string, date: Date) {
     const startOfTheDay = dayjs(date).startOf('date')
@@ -26,6 +27,10 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return this.items
       .filter(item => item.user_id === userId)
       .slice((page - 1) * 20, page * 20)
+  }
+
+  async countByUserId(userId: string) {
+    return this.items.filter((item) => item.user_id === userId).length
   }
 
   async create(data: Prisma.CheckInUncheckedCreateInput) {
