21fb61ad4b06cf1bd61bb0026a2ed9ef9f693959
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-05-22 22:28:43 -0300

    Config teardown test e2e

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,12 +1,41 @@
+import { PrismaClient } from '@prisma/client';
+import 'dotenv/config'
+import { execSync } from 'node:child_process';
+
+import { randomUUID } from "node:crypto";
 import { Environment } from "vitest";
+
+// postgresql://docker:docker@localhost:5432/apisolid?schema=public
+
+const prisma = new PrismaClient()
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error(
+      "DATABASE_URL environment variable is not set. Please set it in your.env file."
+    )
+  }
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   async setup() {
-    console.log('Starting')
+    const schema = randomUUID()
+    const databaseURL = generateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = databaseURL
+
+    execSync('npx prisma migrate deploy')
 
     return {
-      teardown() { }
+      async teardown() {
+        await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS ${schema} CASCADE`)
+        await prisma.$disconnect()
+      }
     }
   }
 
