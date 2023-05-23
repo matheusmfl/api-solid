08c25d99a63b05308b9f9ec5a112138c2fc4ddc8
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-05-23 18:28:16 -0300

    Create controllers nearby and search, and add routes

---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,21 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms-use-case'
+
+export async function search(req: FastifyRequest, res: FastifyReply) {
+  const searchGymsQuerySchema = z.object({
+    q: z.string(),
+    page: z.coerce.number().min(1).default(1)
+
+  })
+
+  const { page, q } = searchGymsQuerySchema.parse(req.body)
+
+
+  const searchGymsUseCase = makeSearchGymsUseCase()
+  const { gyms } = await searchGymsUseCase.execute({ page, query: q })
+
+  return res.status(200).send({
+    gyms
+  })
+}
--- src/use-cases/factories/make-fetch-nearby-gyms-use-case.ts
+++ src/use-cases/factories/make-fetch-nearby-gyms-use-case.ts
@@ -1,7 +1,7 @@
 import { FetchNearbyGymsUseCase } from '../fetch-nearby-gyms'
 import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-repository'
 
-export function ma() {
+export function makeFetchNearbyGymsUseCase() {
   const GymsRepository = new PrismaGymsRepository()
   const useCase = new FetchNearbyGymsUseCase(GymsRepository)
 
